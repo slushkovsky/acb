6af61438978c9bf201acdf167221501c21e68a5f
Sergey Lushkovsky <s.lushkovsky@gmail.com>
2016-09-11 19:56:39 +0300

    Added instructors list on click action.

---
+++ server/views/partner/places_list.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
+
+from server.models import Instructor
+
+
+@require_http_methods('GET')
+@login_required
+def instructors_list_view(request):
+    return render(request, 'instructors_list/list.html', {
+        'employees': Instructor.objects.all()
+    })
